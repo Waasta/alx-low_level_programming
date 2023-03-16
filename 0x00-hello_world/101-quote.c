85abd4f01859e5cecadace948fa344e72532f2da
Waasta <adam.seff@hotmail.com>
2023-03-16 07:32:21 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	/* Your code here */
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }

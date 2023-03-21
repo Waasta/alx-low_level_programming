39fc7bdf7e27fa8d5107d6f81d2effa33f329c2e
Waasta <adam.seff@hotmail.com>
2023-03-21 11:45:50 -0700

    c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,12 @@
 
 
 /**
- * print_alphabet - print all alphabet in lowercase
+* print_alphabet - print all alphabet in lowercase
 */
-
 
 void print_alphabet(void)
 {
 	char letter;
-
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	
